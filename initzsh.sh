f4a864ac8cf3f5f34335cad03e8bd70344075c28
LiuTianjie <liu28719976@gmail.com>
2022-11-01 17:31:17 +0800

    add:zsh config shell script

---
+++ initzsh.sh
@@ -0,0 +1,7 @@
+sudo apt install zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
